ee3cb767c1c758581b1180774b3968e453c8c48d
Sergio <onceceroseis@gmail.com>
2025-11-27 12:01:16 +0100

    Actualizar index.html y mover imágenes a public/images

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 // CÓDIGOS VÁLIDOS
 const codigosValidos = {
